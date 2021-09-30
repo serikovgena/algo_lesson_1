b5b14b694e3e37d81937e8aad1795f1b09b84a2b
GennadiySerikov <serikov.gen@yandex.ru>
2021-09-30 17:15:36 +0300

    feat: add Numbers.css. isPrime method include

---
+++ lesson_1/Number.cs
@@ -0,0 +1,22 @@
+﻿namespace lesson_1
+{
+    internal static class Number
+    {
+        static public bool IsPrime(int number) => HowManyDivisorsIn(number) == 2;
+        static public int HowManyDivisorsIn(int number)
+        {
+            int divisors = 0;
+            int it = 1;
+
+            while (it <= number)
+            {
+                if((number % it) == 0)
+                {
+                    divisors++;
+                }
+                it++;
+            }
+            return divisors;
+        }
+    }
+}
